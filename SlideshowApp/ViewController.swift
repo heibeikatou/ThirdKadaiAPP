b38cd6d4c8badba841fb193bd6ef1a5145630836
DT.Products <dandt.products@gmail.com>
2017-02-02 18:46:30 +0900

    レッスン５の課題

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,14 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Window_Imageview: UIImageView!
+    
+    @IBOutlet weak var btn_back: UIButton!
+    @IBOutlet weak var btn_PlayStop: UIButton!
+    @IBOutlet weak var btn_next: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //初期画面設定関数
+        init_ImageView()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func init_ImageView() {
+        //インスタンスの作成
+        let img = UIImage(named:"1")
+        //Storyboardへの表示
+        self.Window_Imageview.image = img
+    
+    }
+    
+    @IBAction func BackImage(_ sender: Any) {
+    }
+    @IBAction func PlayStop(_ sender: Any) {
+    }
+    @IBAction func NextImage(_ sender: Any) {
     }
 
 
